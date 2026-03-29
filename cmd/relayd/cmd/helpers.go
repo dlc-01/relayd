31c1fb397486cb355e9a4e7fe878126a9d9404bf
DLC <shabrov23.02@gmail.com>
2026-03-29 17:37:50 +0300

    fix(cli): pass flags to env before LoadClientConfig

--- cmd/relayd/cmd/helpers.go
+++ cmd/relayd/cmd/helpers.go
@@ -8,23 +8,24 @@ import (
 )
 
 func bindEnv(cmd *cobra.Command, flag, env string) {
-	if !cmd.Flags().Changed(flag) {
-		if val := os.Getenv(env); val != "" {
-			cmd.Flags().Set(flag, val)
-		}
+	if cmd.Flags().Changed(flag) {
+		val, _ := cmd.Flags().GetString(flag)
+		os.Setenv(env, val)
+	} else if val := os.Getenv(env); val != "" {
+		cmd.Flags().Set(flag, val)
 	}
 }
 
 func bindEnvBool(cmd *cobra.Command, flag, env string) {
-	if !cmd.Flags().Changed(flag) {
-		if val := os.Getenv(env); val == "true" {
-			cmd.Flags().Set(flag, "true")
-		}
+	if cmd.Flags().Changed(flag) {
+		os.Setenv(env, "true")
+	} else if val := os.Getenv(env); val == "true" {
+		cmd.Flags().Set(flag, "true")
 	}
 }
 
 func bindTunnels(tunnels []string) {
-	if os.Getenv("RELAYD_TUNNELS") == "" {
+	if len(tunnels) > 0 {
 		os.Setenv("RELAYD_TUNNELS", strings.Join(tunnels, ","))
 	}
 }
